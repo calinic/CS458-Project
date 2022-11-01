a58c0021311c4b773d8fe54b6eb3602f75982843
calinic <nnnellum@gmail.com>
2022-11-01 13:59:14 -0700

    Animations for walking and attacking have been added. Using map and graphics from tutorial for stand in while ours are under development.

--- level.py
+++ level.py
@@ -3,6 +3,8 @@ from settings import *
 from tile import Tile
 from player import Player
 from debug import debug 
+from support import *
+from random import choice, random
 
 class Level:
 	def __init__(self):
@@ -18,20 +20,43 @@ class Level:
 		self.create_map()
 
 	def create_map(self):
-		for row_index,row in enumerate(WORLD_MAP):
-			for col_index, col in enumerate(row):
-				x = col_index * TILESIZE
-				y = row_index * TILESIZE
-				if col == 'x':
-					Tile((x,y),[self.visible_sprites,self.obstacle_sprites])
-				if col == 'p':
-					self.player = Player((x,y),[self.visible_sprites],self.obstacle_sprites)
+		layouts = {
+			'boundary': import_csv_layout('./map/map_FloorBlocks.csv'),
+			'grass': import_csv_layout('./map/map_Grass.csv'),
+			'object': import_csv_layout('./map/map_Objects.csv'),
+		}
+		grpahics = {
+			'grass': import_folder('./graphics/grass'),
+			'objects': import_folder('./graphics/objects'),
+		}
+
+		for style,layout in layouts.items():
+			for row_index,row in enumerate(layout):
+				for col_index, col in enumerate(row):
+					if col != '-1':
+						x = col_index * TILESIZE
+						y = row_index * TILESIZE
+						#creating invisible boundary tiles 
+						if style == 'boundary':
+							Tile((x,y),[self.obstacle_sprites],'invisible')
+						#creating grass tiles 
+						if style == 'grass':
+							random_grass_image = choice(grpahics['grass'])
+							Tile((x,y),[self.visible_sprites, self.obstacle_sprites],'grass',random_grass_image)				
+						if style == 'object':
+							surf = grpahics['objects'][int(col)]
+							Tile((x,y),[self.visible_sprites, self.obstacle_sprites],'objects',surf)
+	#			if col == 'x':
+	#				Tile((x,y),[self.visible_sprites,self.obstacle_sprites])
+	#			if col == 'p':
+		self.player = Player((2000,1430),[self.visible_sprites],self.obstacle_sprites)
 
 
 	def run(self):
 		# update and draw game 
 		self.visible_sprites.custom_draw(self.player)
 		self.visible_sprites.update()
+		debug(self.player.status)
 
 class YSortCameraGroup(pygame.sprite.Group):
 	def __init__(self):
@@ -43,10 +68,18 @@ class YSortCameraGroup(pygame.sprite.Group):
 		self.half_height = self.display_surface.get_size()[1] // 2
 		self.offset = pygame.math.Vector2()
 
+		#creating the floor 
+		self.floor_surf = pygame.image.load('./graphics/tilemap/ground.png')
+		self.floor_rect = self.floor_surf.get_rect(topleft = (0,0))
+
 	def custom_draw(self,player):
 		#getting offset
 		self.offset.x = player.rect.centerx - self.half_width
 		self.offset.y = player.rect.centery - self.half_height
+
+		#drawing the floor 
+		floor_offset_pos = self.floor_rect.topleft - self.offset
+		self.display_surface.blit(self.floor_surf,floor_offset_pos)
 
 		#for sprite in self.sprites():
 		for sprite in sorted(self.sprites(),key = lambda sprite: sprite.rect.centery):
