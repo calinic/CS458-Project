d4ceab7abbb712df3d5c501f1ac103f07abb2159
calinic <nnnellum@gmail.com>
2022-11-21 00:01:08 -0800

    Player-enemy interaction impleneted. Additional animations as well.

--- level.py
+++ level.py
@@ -5,10 +5,11 @@ from tile import Tile
 from player import Player
 from debug import debug 
 from support import *
-from random import choice, random
+from random import choice, random, randint
 from weapon import Weapon
 from ui import UI
 from enemy import Enemy
+from particles import AnimationPlayer
 
 class Level:
 	def __init__(self):
@@ -22,6 +23,8 @@ class Level:
 
 		#attack sprites
 		self.current_attack = None
+		self.attack_sprites = pygame.sprite.Group()
+		self.attackable_sprites = pygame.sprite.Group()
 
 		# sprite setup 
 		self.create_map()
@@ -29,17 +32,21 @@ class Level:
 		#user interface 
 		self.ui = UI()
 
+		#particles 
+		self.animation_player = AnimationPlayer()
+
 	def create_map(self):
 		layouts = {
 			#'boundary': import_csv_layout('./map/map_FloorBlocks.csv'),
-			'boundary': import_csv_layout('./map/marshmapMkII_ground.csv'),
-			'grass': import_csv_layout('./map/map_Grass.csv'),
-			'object': import_csv_layout('./map/map_Objects.csv'),
-			'entities': import_csv_layout('./map/map_Entities.csv')
+			'boundary': import_csv_layout('./nic_map/nic_BoundaryBlocks.csv'),
+			'grass': import_csv_layout('./nic_map/nic_Grass.csv'),
+			'object': import_csv_layout('./nic_map/nic_Objects.csv'),
+			'entities': import_csv_layout('./nic_map/nic_Entities.csv')
 		}
 		grpahics = {
-			'grass': import_folder('./graphics/grass'),
-			'objects': import_folder('./graphics/objects'),
+			#'grass': import_folder('./graphics/grass'),
+			'grass': import_folder('./nic_map/grass'),
+			'objects': import_folder('./nic_map/objects'),
 		}
 
 		for style,layout in layouts.items():
@@ -56,7 +63,7 @@ class Level:
 						#creating grass tiles 
 						if style == 'grass':
 							random_grass_image = choice(grpahics['grass'])
-							Tile((x,y),[self.visible_sprites, self.obstacle_sprites],'grass',random_grass_image)				
+							Tile((x,y),[self.visible_sprites, self.obstacle_sprites,self.attackable_sprites],'grass',random_grass_image)				
 						
 						if style == 'object':
 							surf = grpahics['objects'][int(col)]
@@ -76,10 +83,16 @@ class Level:
 								elif col == '391': monster_name = 'spirit'
 								elif col == '392': monster_name = 'raccoon'
 								else: monster_name = 'squid'
-								Enemy(monster_name,(x,y),[self.visible_sprites],self.obstacle_sprites)
+								Enemy(
+									monster_name,
+									(x,y),
+									[self.visible_sprites,self.attackable_sprites],
+									self.obstacle_sprites,
+									self.damage_player,
+									self.trigger_death_particles)
 
 	def create_attack(self):
-		self.current_attack = Weapon(self.player,[self.visible_sprites])
+		self.current_attack = Weapon(self.player,[self.visible_sprites,self.attack_sprites])
 	
 	def create_magic(self,style,strength,cost):
 		print(style)
@@ -91,11 +104,37 @@ class Level:
 			self.current_attack.kill()
 		self.current_attack = None
 
+	def player_attack_logic(self):
+		if self.attack_sprites:
+			for attack_sprite in self.attack_sprites:
+				collision_sprites = pygame.sprite.spritecollide(attack_sprite,self.attackable_sprites,False)
+				if collision_sprites:
+					for target_sprite in collision_sprites:
+						if target_sprite.sprite_type == 'grass':
+							pos = target_sprite.rect.center
+							offset = pygame.math.Vector2(0,75)
+							for leaf in range(randint(3,6)):
+								self.animation_player.create_grass_particles(pos - offset,[self.visible_sprites])
+							target_sprite.kill()
+						else:
+							target_sprite.get_damage(self.player,attack_sprite.sprite_type)
+	
+	def damage_player(self,amount,attack_type):
+		if self.player.vulnerable:
+			self.player.health -= amount
+			self.player.vulnerable = False
+			self.player.hurt_time = pygame.time.get_ticks()
+			self.animation_player.create_particles(attack_type,self.player.rect.center,[self.visible_sprites])
+
+	def trigger_death_particles(self,pos,particle_type):
+		self.animation_player.create_particles(particle_type,pos,self.visible_sprites)
+
 	def run(self):
 		# update and draw game 
 		self.visible_sprites.custom_draw(self.player)
 		self.visible_sprites.update()
 		self.visible_sprites.enemy_update(self.player)
+		self.player_attack_logic()
 		self.ui.display(self.player)
 		#debug(self.player.status)
 
@@ -110,8 +149,8 @@ class YSortCameraGroup(pygame.sprite.Group):
 		self.offset = pygame.math.Vector2()
 
 		#creating the floor 
-		#self.floor_surf = pygame.image.load('./graphics/tilemap/ground.png')
-		self.floor_surf = pygame.image.load('./graphics/tilemap/MarshMapMkII.png')
+		self.floor_surf = pygame.image.load('./nic_map/64Map.png')
+		#self.floor_surf = pygame.image.load('./graphics/tilemap/MarshMapMkII.png')
 		self.floor_rect = self.floor_surf.get_rect(topleft = (0,0))
 
 	def custom_draw(self,player):
