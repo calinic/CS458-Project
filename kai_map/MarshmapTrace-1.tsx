a647ce412ebd0e7bacd011007dd4632b0a376c6d
calinic <nnnellum@gmail.com>
2022-11-20 23:59:19 -0800

    Able to work with our maps now. More additions with player enemy interaction and some animations.

---
+++ kai_map/MarshmapTrace-1.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.9" tiledversion="1.9.2" name="MarshmapTrace-1" tilewidth="128" tileheight="128" tilecount="299" columns="12">
+ <image source="MarshmapTrace-1.png" width="1654" height="2339"/>
+</tileset>
---
+++ nic_map/Details64.tsx
@@ -0,0 +1,12 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.9" tiledversion="1.9.2" name="Details" tilewidth="64" tileheight="64" tilecount="80" columns="16">
+ <image source="tilemap/details.png" width="1024" height="320"/>
+ <tile id="32" probability="0.75"/>
+ <tile id="33" probability="0.75"/>
+ <tile id="34" probability="0.75"/>
+ <tile id="35" probability="0.75"/>
+ <tile id="36" probability="0.75"/>
+ <tile id="37" probability="0.75"/>
+ <tile id="38" probability="0.75"/>
+ <tile id="39" probability="0.75"/>
+</tileset>
