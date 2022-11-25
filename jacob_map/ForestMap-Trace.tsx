a2112d5c4e02092d08de478695d8f04bec3bf2eb
jarsena <declanarsenault@yahoo.com>
2022-11-25 15:36:50 -0600

    Forest Map (1st draft)
    Added forest map, let me know if you think it needs changes

---
+++ jacob_map/ForestMap-Trace.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.9" tiledversion="1.9.2" name="ForestMap-Trace" tilewidth="128" tileheight="128" tilecount="36" columns="9">
+ <image source="ForestMap-Trace.png" width="1170" height="616"/>
+</tileset>
