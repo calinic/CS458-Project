8d87e3c97669b9af94dcc48b1d3dbe858edc6a13
jarsena <declanarsenault@yahoo.com>
2022-11-29 10:21:33 -0800

    Forest Map Mk II
    actual boundary blocks and player spawn + some grass details

---
+++ jacob_map/128ForestDetails.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.9" tiledversion="1.9.2" name="128ForestDetails" tilewidth="128" tileheight="128" tilecount="8" columns="8">
+ <image source="128ForestDetails.png" width="1024" height="128"/>
+</tileset>
